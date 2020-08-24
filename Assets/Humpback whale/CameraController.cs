b33e10f224b0c20873dffde84c9267864416749d
dhtpwls0170@naver.com <dhtpwls0170@naver.com>
2020-08-24 09:12:47 +0900

    floackingtest

---
+++ Assets/Humpback whale/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSmoothingFactor = 1;
+    public float lookUpMax = 60;
+    public float lookUpMin = -60;
+
+    private Quaternion camRoation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camRoation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camRoation.x += Input.GetAxis("Mouse Y") * cameraSmoothingFactor*(-1);   // look up/down
+        camRoation.y += Input.GetAxis("Mouse X") * cameraSmoothingFactor;   // look left/right
+
+        camRoation.x = Mathf.Clamp(camRoation.x, lookUpMin, lookUpMax);
+        transform.localRotation = Quaternion.Euler(camRoation.x, camRoation.y, camRoation.z);
+    }
+}
---
+++ Assets/Humpback whale/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+    Vector3 newVelocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
+    }
+}
